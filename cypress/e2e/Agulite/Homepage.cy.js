dbf735f42e2f5a3c47e95204e1ce1038a2b18dff
unknown <sundaystanley56@gmail.com>
2022-08-07 20:25:28 +0100

    Added other feartures

--- cypress/e2e/Agulite/Homepage.cy.js
+++ cypress/e2e/Agulite/Homepage.cy.js
@@ -1,6 +1,7 @@
+/// <reference types="cypress"/>
 describe('NavBar Menu', () => {
   before(() => {
-    cy.visit('https://agulite.com')
+    cy.visit('/')
   })
 
   it('Should be able to click the home page', () => {
@@ -9,6 +10,7 @@ describe('NavBar Menu', () => {
     cy.get(':nth-child(1) > .nav-link').click()
     cy.url().should('eq', 'https://agulite.com/')
   })
+
   it('Should be able to navigate to the Talent Aquasition tab', ()=>{
     cy.get(':nth-child(2) > .nav-link').should(
       'have.attr',
@@ -16,19 +18,30 @@ describe('NavBar Menu', () => {
       'https://talentsurvey.agulite.com'
     )
   })
-  it('Should be able to navigate to the Career page', ()=>{
-   cy.get(':nth-child(3) > .nav-link').should(
-     'have.attr',
-     'href',
-     'https://techcareer.agulite.com'
-   )
+
+it('Should be able to navigate to the Career page', ()=>{
+  cy.get(':nth-child(3) > .nav-link').should(
+    'have.attr',
+    'href',
+    'https://techcareer.agulite.com'
+  )
   })
-  it('Should be able to navigate to the Expert', ()=>{
-   cy.get(':nth-child(4) > .nav-link').should(
-     'have.attr',
-     'href',
-     'https://expert.agulite.com'
-   )
-   cy.screenshot('myApp')
+
+it('Should be able to navigate to the Expert', ()=>{
+  cy.get(':nth-child(4) > .nav-link').should(
+    'have.attr',
+    'href',
+    'https://expert.agulite.com'
+  )
+  cy.screenshot('myApp')
+  })
+})
+
+describe('Body Check', ()=>{
+  before(()=>cy.visit('/'))
+it('should have only one "H1"', ()=>{
+  cy.get('h1 > .turquoise')
+    .should('have.class', 'turquoise')
+    .and('have.css', 'color', 'rgb(245, 128, 12)')
   })
 })
